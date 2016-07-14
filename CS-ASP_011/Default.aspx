d2ef2474a36b8c5734590f03c43cd5649ef40357
Jennyf19 <ferriesj@gmail.com>
2016-07-14 12:59:57 -0700

    added checked box

---
+++ CS-ASP_011/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_011.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        Is
+        <asp:TextBox ID="firstTextBox" runat="server"></asp:TextBox>
+&nbsp; equal to&nbsp;
+        <asp:TextBox ID="secondTextBox" runat="server"></asp:TextBox>
+&nbsp; ?<br />
+        <br />
+        <asp:CheckBox ID="catcheckBox" runat="server" OnCheckedChanged="catcheckBox_CheckedChanged" Text="Do you like cats?" />
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" BackColor="#3399FF" BorderStyle="Groove" OnClick="okButton_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="catLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_011/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_011
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            if (firstTextBox.Text == secondTextBox.Text)
+            {
+                resultLabel.Text = "Yes! They're equal!";
+            }
+            else
+            {
+                resultLabel.Text = "These two values are not equal.";
+
+            }
+        }
+
+        protected void catcheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (catcheckBox.Checked == true)
+
+            {
+                catLabel.Text = "Yes! I like cats too!";
+            }
+            else
+            {
+                catLabel.Text = "Cats are amazing";
+            }
+        }
+    }
+}
